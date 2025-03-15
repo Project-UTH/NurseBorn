32c84d6c89a11c917f669c850331a04256627744
fujiwara-hia <thinhnguyencong158@gmail.com>
2025-03-15 22:27:11 +0700

    Models: Feedbacks

---
+++ src/main/java/edu/uth/nurseborn/models/Feedbacks.java
@@ -0,0 +1,111 @@
+package edu.uth.nurseborn.models;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.Instant;
+
+@Entity
+public class Feedbacks {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "feedback_id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "booking_id", nullable = false)
+    private Bookings booking;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "family_user_id", nullable = false)
+    private Users familyUser;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "nurse_user_id", nullable = false)
+    private Users nurseUser;
+
+    @Column(name = "rating")
+    private Integer rating;
+
+    @Lob
+    @Column(name = "comment")
+    private String comment;
+
+    @Lob
+    @Column(name = "response")
+    private String response;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Bookings getBooking() {
+        return booking;
+    }
+
+    public void setBooking(Bookings booking) {
+        this.booking = booking;
+    }
+
+    public Users getFamilyUser() {
+        return familyUser;
+    }
+
+    public void setFamilyUser(Users familyUser) {
+        this.familyUser = familyUser;
+    }
+
+    public Users getNurseUser() {
+        return nurseUser;
+    }
+
+    public void setNurseUser(Users nurseUser) {
+        this.nurseUser = nurseUser;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+}
