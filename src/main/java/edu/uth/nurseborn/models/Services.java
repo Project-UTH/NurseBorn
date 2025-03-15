9ccca363bb5a98f1868f89d65682edf3b787bec2
MinhDat8386 <dattm4268@.ut.edu.vn>
2025-03-15 22:06:44 +0700

    Models :  Services

--- src/main/java/edu/uth/nurseborn/models/Services.java
+++ src/main/java/edu/uth/nurseborn/models/Services.java
@@ -1,7 +1,97 @@
 package edu.uth.nurseborn.models;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.math.BigDecimal;
 
 @Entity
 public class Services {
-  }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "service_id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "nurse_user_id", nullable = false)
+    private Users nurseUser;
+
+    @Column(name = "service_name", nullable = false, length = 100)
+    private String serviceName;
+
+    @Lob
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+
+    @Lob
+    @Column(name = "availability_schedule", nullable = false)
+    private String availabilitySchedule;
+
+    @ColumnDefault("'active'")
+    @Lob
+    @Column(name = "status")
+    private String status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Users getNurseUser() {
+        return nurseUser;
+    }
+
+    public void setNurseUser(Users nurseUser) {
+        this.nurseUser = nurseUser;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getAvailabilitySchedule() {
+        return availabilitySchedule;
+    }
+
+    public void setAvailabilitySchedule(String availabilitySchedule) {
+        this.availabilitySchedule = availabilitySchedule;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
