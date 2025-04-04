939bec3bc22ce488c594d913f1a87dfb70577852
Binh-Nguyen <binhb3573@gmail.com>
2025-04-04 15:23:10 +0700

    creat AuthController

---
+++ src/main/java/edu/uth/nurseborn/controllers/auth/AuthController.java
@@ -0,0 +1,29 @@
+package edu.uth.nurseborn.controllers.auth;
+
+import edu.uth.nurseborn.dtos.LoginRequestDTO;
+import edu.uth.nurseborn.dtos.LoginResponseDTO;
+import edu.uth.nurseborn.dtos.UserDTO;
+import edu.uth.nurseborn.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody UserDTO userDTO) {
+        userService.registerUser(userDTO);
+        return ResponseEntity.ok("Đăng ký thành công");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO loginRequest) {
+        LoginResponseDTO response = userService.login(loginRequest);
+        return ResponseEntity.ok(response);
+    }
+}
